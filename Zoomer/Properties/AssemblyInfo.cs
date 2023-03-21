987517005389895253bd62bf799cb8c5e5649930
Lawrence <30965946+Halbann@users.noreply.github.com>
2023-03-21 10:21:08 +0000

    assembly name

--- Zoomer/Properties/AssemblyInfo.cs
+++ Zoomer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UIScaler")]
+[assembly: AssemblyTitle("Zoomer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UIScaler")]
+[assembly: AssemblyProduct("Zoomer")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
